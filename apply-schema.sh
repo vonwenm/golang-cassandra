0d2f4c72ed2b9e0828636c8dd0efad0046920221
Casper devboxknuth <devboxknuth@springer.com>
2013-06-12 11:59:06 +0100

    Susie fixed spelling mistake in path to apply-cass-schema

--- apply-schema.sh
+++ apply-schema.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sh scrips/apply-cass-schema.sh localhost
+sh scripts/apply-cass-schema.sh localhost
 
 if [ "$?" -ne "0" ]; then
   echo "Applying the schema to Cassandra has failed. Please check Cassandra is running, or execute the install-cassandra.sh script at project root."
