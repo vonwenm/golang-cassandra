c44b308865332c79048881c8db042605c6e52700
babbage <babbage@example.com>
2014-07-10 10:10:43 +0100

    Cris/Hristo removed apply schema from project root too (use apply steps)

--- apply-schema.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sh scripts/apply-cass-schema.sh localhost
-
-if [ "$?" -ne "0" ]; then
-  echo "Applying the schema to Cassandra has failed. Please check Cassandra is running, or execute the install-cassandra.sh script at project root."
-  exit 1
-fi
