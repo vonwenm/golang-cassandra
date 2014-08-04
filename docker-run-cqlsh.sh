ed57c9a18658a3835cefde6f80f19e9ae7e0a1ec
squirrel02 <casper@springer.com>
2014-08-04 17:30:07 +0100

    Cris updated scripts to use the docker image from cassandra-track repo

--- docker-run-cqlsh.sh
+++ docker-run-cqlsh.sh
@@ -1,2 +1,2 @@
 CASSANDRA_CONTAINER_IP=`docker inspect cass1 |grep IPA|cut -f4 -d'"'`
-docker run -ti --rm track/cassandra cqlsh --cql3 $CASSANDRA_CONTAINER_IP
+docker run -ti --rm springersbm/cassandra-track cqlsh --cql3 $CASSANDRA_CONTAINER_IP
